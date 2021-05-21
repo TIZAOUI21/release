f39d7924b8401da54bcbd168fa297d38b641dc87
AYMEN <tizaouiaymen3@gmail.com>
2021-05-21 01:47:11 -0400

    mise en forme heritage

---
+++ cmake-build-debug/CMakeFiles/release.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Clioncode/release/cout.cpp" "D:/Clioncode/release/cmake-build-debug/CMakeFiles/release.dir/cout.cpp.obj"
+  "D:/Clioncode/release/eau.cpp" "D:/Clioncode/release/cmake-build-debug/CMakeFiles/release.dir/eau.cpp.obj"
+  "D:/Clioncode/release/electricite.cpp" "D:/Clioncode/release/cmake-build-debug/CMakeFiles/release.dir/electricite.cpp.obj"
+  "D:/Clioncode/release/main.cpp" "D:/Clioncode/release/cmake-build-debug/CMakeFiles/release.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
